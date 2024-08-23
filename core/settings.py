f71e89bb0a50b1acac0a0b042309bdb04e691775
jona44 <tjmanyadza@live.com>
2024-08-23 21:24:10 +0200

    modified:   core/__pycache__/settings.cpython-311.pyc 	modified:   core/settings.py 	modified:   db.sqlit3

--- core/settings.py
+++ core/settings.py
@@ -163,3 +163,12 @@ TEMPLATE_DIRS = [(BASE_DIR, 'templates')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = os.getenv('EMAIL_HOST')
+# EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
