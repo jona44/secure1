822e7c55854d9d360ea3bed6ac760b5648fd1d85
jona44 <tjmanyadza@live.com>
2024-08-26 20:52:11 +0200

    modified:   .gitignore 	modified:   core/settings.py 	new file:   media/school_logos/highschoollogo.jpeg 	new file:   static/media/school_logos/classes.png 	new file:   static/media/school_logos/highschoollogo.jpeg 	new file:   static/media/school_logos/highschoollogo_nkLJFfG.jpeg 	new file:   static/media/school_logos/teacher.png

--- core/settings.py
+++ core/settings.py
@@ -157,10 +157,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, ),]
 STATIC_ROOT = BASE_DIR/'static'
 
-
-
-    
-
 LOGIN_REDIRECT_URL = 'login_redirect'
 LOGOUT_REDIRECT_URL = 'login'
 
